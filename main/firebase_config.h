58766af380aed3902c22548fc2b6602cc19552e3
Boonpin97 <pohboonpin123@hotmail.com>
2024-04-19 19:40:54 +0800

    first commit

---
+++ main/firebase_config.h
@@ -0,0 +1,13 @@
+// Wifi Credentials
+#define SSID "POCOX3"
+#define PASSWORD "01234567"
+
+// Read readme.md to properly configure api key and authentication
+
+// create a new api key and add it here 
+#define API_KEY "AIzaSyAXNh3WFGrR2djHHuM2JU3uHeu1XG1ywEA"
+// Copy your firebase real time database link here 
+#define DATABASE_URL "https://plant-c01cb-default-rtdb.asia-southeast1.firebasedatabase.app/"  
+
+#define USER_EMAIL "admin@admin.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "admin123"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
